f607966707ad96b70fc5c4321c2aa3cbf79d60c5
jafinol <jafinol@gmail.com>
2021-03-08 04:56:05 +0000

    modal commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-dark bg-dark text-white ">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">4GeeksAcademy</span>
-			</Link>
-			<div className="ml-auto">
-				<button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-					Login
-				</button>
-			</div>
-		</nav>
+		<div>
+			<nav className="navbar navbar-dark bg-dark text-white ">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">4GeeksAcademy</span>
+				</Link>
+				<div className="ml-auto text-dark">
+					<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+						Login
+					</button>
+
+					<div
+						className="modal fade text-dark"
+						id="exampleModal"
+						role="dialog"
+						aria-labelledby="exampleModalLabel"
+						aria-hidden="true">
+						<div className="modal-dialog" role="document">
+							<div className="modal-content">
+								<div className="modal-header">
+									<h5 className="modal-title  text-dark" id="exampleModalLabel">
+										Login
+									</h5>
+								</div>
+								<div className="modal-body text-dark">
+									<div className="row">
+										<div className="col-md-5">
+											<input
+												type="text"
+												className="form-control"
+												id="username"
+												placeholder="username"
+												value=""
+												required
+											/>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-md-5">
+											<input
+												type="text"
+												className="form-control"
+												id="Password"
+												placeholder="Password"
+												value=""
+												required
+											/>
+										</div>
+									</div>
+								</div>
+								<div className="modal-footer">
+									<button type="button" className="btn btn-secondary" data-dismiss="modal">
+										Close
+									</button>
+									<button type="button" className="btn btn-primary">
+										Login
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import $ from "jquery";
+import Popper from "popper.js";
+
 //include bootstrap npm library into the bundle
+
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //include your index.scss file into the bundle
+
 import "../styles/index.scss";
 
 //import your own components
 import Layout from "./layout";
-
+//import "semantic-ui-css/semantic.min.css";
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
